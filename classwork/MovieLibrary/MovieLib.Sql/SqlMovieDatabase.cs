a94c827cab992ae04e9adec2e2e7fed997b007ae
Alamont912 <thomas.lupinacci077@my.tccd.edu>
2022-04-28 20:49:36 -0500

    Finished databases.

--- classwork/MovieLibrary/MovieLib.Sql/SqlMovieDatabase.cs
+++ classwork/MovieLibrary/MovieLib.Sql/SqlMovieDatabase.cs
@@ -15,7 +15,44 @@ namespace MovieLib.Sql
 
         private readonly string _connectionString;
 
-        protected override Movie AddCore ( Movie movie ) => throw new NotImplementedException();
+        protected override Movie AddCore ( Movie movie )
+        {
+            using(var conn = OpenConnection())
+            {
+                var cmd = new SqlCommand("AddMovie", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                //Add Parameters
+
+                //Approach 1 - preferred for SQL
+                cmd.Parameters.AddWithValue("@name", movie.Title);
+                cmd.Parameters.AddWithValue("@releaseYear", movie.ReleaseYear);
+                cmd.Parameters.AddWithValue("@runLength", movie.Duration);
+                cmd.Parameters.AddWithValue("@isClassic", movie.IsClassic);
+
+                //Approach 2 - convert primative to SQL type
+                var parmRating = cmd.Parameters.Add("@rating", SqlDbType.NVarChar);      
+                parmRating.Value = movie.Rating;
+
+                //Approach 3 - need complete control of parameter or access after call
+                var parmDescription = new SqlParameter("@description", movie.Description);
+                cmd.Parameters.Add(parmDescription);
+
+                //Approach 4 - needs to be generic
+                var parmGenre = cmd.CreateParameter();
+                parmGenre.ParameterName = "@genre";
+                parmGenre.DbType = DbType.String;
+                parmGenre.Value = movie.Genre;
+                cmd.Parameters.Add(parmGenre);
+
+                var result = cmd.ExecuteScalar();   //execute, but give me back the first thing
+
+                movie.Id = Convert.ToInt32(result); //first thing is ID, convert and apply ID
+
+                return movie;   //return movie
+            };
+        }
+
         protected override void DeleteCore ( int id )
         {
             using(var conn = OpenConnection())
@@ -33,10 +70,28 @@ namespace MovieLib.Sql
                 cmd.ExecuteNonQuery();
             }
         }
+
         protected override Movie FindByName ( string name )
         {
-            return null;
+            //Streamed read
+            using (var conn = OpenConnection())
+            {
+                var cmd = new SqlCommand("FindByName", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@name", name);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())    //what actually reads the next row of data
+                    {
+                        return LoadMovie(reader);
+                    };
+                };
+            };
+            return null;    //return at least 1 row, or return nothing
         }
+
         protected override IEnumerable<Movie> GetAllCore ()
         {
             DataSet ds = new DataSet();
@@ -83,7 +138,71 @@ namespace MovieLib.Sql
         }
 
         //TODO: GetCore
-        protected override Movie GetCore ( int id ) => GetAllCore().FirstOrDefault(x => x.Id == id);
-        protected override void UpdateCore ( int id, Movie movie ) => throw new NotImplementedException();
+        protected override Movie GetCore ( int id )
+        {
+            //Streamed read
+            using(var conn = OpenConnection())
+            {
+                var cmd = new SqlCommand("GetMovie", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@id", id);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while(reader.Read())    //what actually reads the next row of data
+                    {
+                        return LoadMovie(reader);
+                    };
+                };
+            };
+            return null;    //return at least 1 row, or return nothing
+        }
+
+        private static Movie LoadMovie ( SqlDataReader reader ) => new Movie() {
+            Id = Convert.ToInt32(reader[0]),                       //Array-based index and convert
+            Title = reader["Name"]?.ToString(),                    //Array-based column name and convert
+            Description = reader.IsDBNull(2) ? "" : reader.GetFieldValue<string>(2),         //Field-based index
+            Duration = reader.GetFieldValue<int>("RunLength"),     //Field-based column names
+            Rating = reader.GetString(3),                          //Old, type-based ordinal overloads
+            ReleaseYear = reader.GetInt32("ReleaseYear"),
+            Genre = reader.GetFieldValue<string>("Genre"),
+            IsClassic = reader.GetFieldValue<bool>("IsClassic"),
+        }; 
+
+        protected override void UpdateCore ( int id, Movie movie )
+        {
+            using (var conn = OpenConnection())
+            {
+                var cmd = new SqlCommand("UpdateMovie", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                //Add Parameters
+
+                //Approach 1 - preferred for SQL
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@name", movie.Title);
+                cmd.Parameters.AddWithValue("@releaseYear", movie.ReleaseYear);
+                cmd.Parameters.AddWithValue("@runLength", movie.Duration);
+                cmd.Parameters.AddWithValue("@isClassic", movie.IsClassic);
+
+                //Approach 2 - convert primative to SQL type
+                var parmRating = cmd.Parameters.Add("@rating", SqlDbType.NVarChar);
+                parmRating.Value = movie.Rating;
+
+                //Approach 3 - need complete control of parameter or access after call
+                var parmDescription = new SqlParameter("@description", movie.Description);
+                cmd.Parameters.Add(parmDescription);
+
+                //Approach 4 - needs to be generic
+                var parmGenre = cmd.CreateParameter();
+                parmGenre.ParameterName = "@genre";
+                parmGenre.DbType = DbType.String;
+                parmGenre.Value = movie.Genre;
+                cmd.Parameters.Add(parmGenre);
+
+                cmd.ExecuteNonQuery();
+            };
+        }
     }
 }
